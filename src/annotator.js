0c2b27ebfd161f07ea24154de2d0914ba5a47158
shiftleftorca <101038122+shiftleftorca@users.noreply.github.com>
2022-08-07 22:11:01 +0300

    Update annotator.js

--- src/annotator.js
+++ src/annotator.js
@@ -6,10 +6,8 @@ function extractAnnotations(results) {
     function getDetail(controlResults, file) {
         let details = controlResults.control["details"];
         let recommendation = file["expected_value"];
-        console.log("AYALAA: " + recommendation)
         if (recommendation) {
-            details.concat("\nRecommendation: ", recommendation);
-            console.log("AYALAA: " + details)
+            details = `${details}\nRecommendation: ${recommendation}`
         }
         return details
     }
